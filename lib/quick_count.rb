cdf6dedc246caafa2eeb1218d0325d321334116f
Dale Stevens <dale@getnotion.com>
2018-03-17 17:01:13 -0600

    Improved accuracy and threshold support

--- lib/quick_count.rb
+++ lib/quick_count.rb
@@ -13,44 +13,49 @@ module QuickCount
     ::ActiveRecord::Relation.send :include, CountEstimate::ActiveRecord::Relation
   end
 
-  def self.install
-    ::ActiveRecord::Base.connection.execute(quick_count_sql)
-    ::ActiveRecord::Base.connection.execute(count_estimate_sql)
+  def self.install(threshold: 500000)
+    ::ActiveRecord::Base.connection.execute(quick_count_sql(threshold: threshold))
+    ::ActiveRecord::Base.connection.execute(count_estimate_sql())
   end
 
   def self.uninstall
-    ::ActiveRecord::Base.connection.execute("DROP FUNCTION quick_count(text);")
-    ::ActiveRecord::Base.connection.execute("DROP FUNCTION count_estimate(text);")
+    ::ActiveRecord::Base.connection.execute("DROP FUNCTION IF EXISTS quick_count(text, bigint);")
+    ::ActiveRecord::Base.connection.execute("DROP FUNCTION IF EXISTS quick_count(text);")
+    ::ActiveRecord::Base.connection.execute("DROP FUNCTION IF EXISTS count_estimate(text);")
   end
 
 private
 
-  def self.quick_count_sql
-    <<-eos
-      CREATE OR REPLACE FUNCTION quick_count(table_name text) RETURNS bigint AS
+  def self.quick_count_sql(threshold: 500000)
+    <<-SQL
+      CREATE OR REPLACE FUNCTION quick_count(table_name text, threshold bigint default #{threshold}) RETURNS bigint AS
       $func$
-      DECLARE
-          rec   record;
-          rows  integer;
+      DECLARE count bigint;
       BEGIN
-        RETURN (SELECT SUM(estimate) AS estimate FROM (
-        SELECT
-            SUM(child.reltuples::bigint) AS estimate
-        FROM pg_inherits
-            JOIN pg_class parent            ON pg_inherits.inhparent = parent.oid
-            JOIN pg_class child             ON pg_inherits.inhrelid   = child.oid
-            JOIN pg_namespace nmsp_parent   ON nmsp_parent.oid  = parent.relnamespace
-            JOIN pg_namespace nmsp_child    ON nmsp_child.oid   = child.relnamespace
-        WHERE parent.relname = table_name
-        GROUP BY parent.reltuples
-        UNION SELECT reltuples::bigint AS estimate FROM pg_class where relname=table_name) as tables);
+          EXECUTE 'SELECT
+            CASE
+            WHEN SUM(estimate)::integer < '|| threshold ||' THEN
+              (SELECT COUNT(*) FROM '|| table_name ||')
+            ELSE
+              SUM(estimate)::integer
+            END AS count
+          FROM (
+            SELECT
+                ((SUM(child.reltuples::float)/greatest(SUM(child.relpages::float),1))) * (SUM(pg_relation_size(child.oid))::float / (current_setting(''block_size'')::float)) AS estimate
+            FROM pg_inherits
+                JOIN pg_class parent ON pg_inherits.inhparent = parent.oid
+                JOIN pg_class child  ON pg_inherits.inhrelid  = child.oid
+            WHERE parent.relname = '''|| table_name ||'''
+            UNION SELECT (reltuples::float/greatest(relpages::float, 1)) * (pg_relation_size(pg_class.oid)::float / (current_setting(''block_size'')::float)) AS estimate FROM pg_class where relname='''|| table_name ||'''
+          ) AS tables' INTO count;
+          RETURN count;
       END
       $func$ LANGUAGE plpgsql;
-    eos
+    SQL
   end
 
   def self.count_estimate_sql
-    <<-eos
+    <<-SQL
         CREATE OR REPLACE FUNCTION count_estimate(query text) RETURNS integer AS
         $func$
         DECLARE
@@ -65,7 +70,7 @@ private
             RETURN rows;
         END
         $func$ LANGUAGE plpgsql;
-    eos
+    SQL
   end
 
 end
--- lib/quick_count/active_record/base.rb
+++ lib/quick_count/active_record/base.rb
@@ -7,8 +7,9 @@ module QuickCount
 
       module ClassMethods
 
-        def quick_count
-          result = ::ActiveRecord::Base.connection.execute("SELECT quick_count('#{table_name}')")
+        def quick_count(threshold: nil)
+          threshold = threshold ? ", #{threshold}" : nil
+          result = ::ActiveRecord::Base.connection.execute("SELECT quick_count('#{table_name}'#{threshold})")
           result[0]["quick_count"].to_i
         end
 
